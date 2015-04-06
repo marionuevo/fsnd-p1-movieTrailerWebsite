9bfe9b1ca05e21916d34f2502cdb498493213a2e
marionuevo <marionuevo@gmail.com>
2015-04-06 16:55:36 +0100

    Added up to six movies

--- entertainment_center.py
+++ entertainment_center.py
@@ -6,19 +6,36 @@
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story",
-						"http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-						"https://www.youtube.com/watch?v=vwyZH85NQC4")
+toy_story = media.Movie(
+	"Toy Story",
+    "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+    "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-avatar = media.Movie("Avatar",
-					"http://upload.wikipedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
-					"http://www.youtube.com/watch?v=-9ceBgWV8io")
+avatar = media.Movie(
+	"Avatar",
+    "http://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+    "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
-intouchables = media.Movie("Intouchables",
-						"http://upload.wikimedia.org/wikipedia/en/9/93/The_Intouchables.jpg",
-						"https://www.youtube.com/watch?v=R8wUIez--WE")
+intouchables = media.Movie(
+	"Intouchables",
+    "http://upload.wikimedia.org/wikipedia/en/9/93/The_Intouchables.jpg",
+    "https://www.youtube.com/watch?v=R8wUIez--WE")
 
-moviesList = [toy_story, avatar, intouchables]
-# , vietnam, interestellar, asgood
+vietnam = media.Movie(
+	"Good Morning Vietnam",
+	"http://upload.wikimedia.org/wikipedia/en/d/d0/Good_Morning%2C_Vietnam.jpg",
+	"https://www.youtube.com/watch?v=_V-O6nq92wY")
+
+interestellar = media.Movie(
+	"Interestellar",
+	"http://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",
+	"https://www.youtube.com/watch?v=zSWdZVtXT7E")
+
+asgood = media.Movie(
+	"As Good As It Gets",
+	"http://upload.wikimedia.org/wikipedia/en/d/dc/As_good_as_it_gets.jpg",
+	"https://www.youtube.com/watch?v=rrRl2QQKkI8")
+
+moviesList = [avatar, intouchables, vietnam, interestellar, asgood, toy_story]
 
 fresh_tomatoes.open_movies_page(moviesList)
--- media.py
+++ media.py
@@ -6,13 +6,14 @@
 import webbrowser
 
 class Movie():
-	"""
-	This class defines the properties and methods that Movie instances will have.
-	"""
-	def __init__(self, movie_title, poster_image, trailer_youtube):
-		self.title = movie_title
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
+    """This class defines the properties and methods that Movie instances will have.
 
-	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
+    No attributes.
+    """
+    def __init__(self, movie_title, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
